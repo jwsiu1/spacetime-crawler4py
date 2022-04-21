74259ab3695585c92665b2c56f32597e1bea2812
jwsiu1 <93757281+jwsiu1@users.noreply.github.com>
2022-04-21 11:44:58 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -21,100 +21,110 @@ longest_word = 0
 word_freq = defaultdict(int)
 
 def scraper(url, resp):
-    create_report()
-    # add start urls to list of visited urls
-    visited_urls.add(url)
-    links = extract_next_links(url, resp)
-    create_report()
-    return [link for link in links if is_valid(link)]
+  # create and update report
+  create_report()
+  # add start urls to list of visited urls
+  visited_urls.add(url)
+  links = extract_next_links(url, resp)
+  return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
-    # Implementation required.
-    # url: the URL that was used to get the page
-    # resp.url: the actual url of the page
-    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
-    # resp.error: when status is not 200, you can check the error here, if needed.
-    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
-    #         resp.raw_response.url: the url, again
-    #         resp.raw_response.content: the content of the page!
-    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    
-    # list to store hyperlinks
-    list = []
-    # checks that status code is 200
-    if resp.status != 200:
-        # check response code error
-        return list
-    # use BeautifulSoup to extract links
-    soup = BeautifulSoup(resp.raw_response.content, 'lxml')
-    if soup is not None:
-      tokenize(url, soup)
-    for link in soup.find_all('a'):
-        l = link.get('href')
-        # makes sure link is valid
-        if l is not None:
-            # remove url fragment
-            l_defrag = l.split("#")
-            # checks for duplication and if url can be crawled
-            if l_defrag[0] not in visited_urls and is_valid(l_defrag[0]):
-                visited_urls.add(l_defrag[0])
-                list.append(l_defrag[0])
-    #set_list = set(list)
-    #list = [a for a in set_list]
-    return list
+  # Implementation required.
+  # url: the URL that was used to get the page
+  # resp.url: the actual url of the page
+  # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
+  # resp.error: when status is not 200, you can check the error here, if needed.
+  # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
+  #         resp.raw_response.url: the url, again
+  #         resp.raw_response.content: the content of the page!
+  # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+
+  # list to store hyperlinks
+  list = []
+  # checks for valid response status (200)
+  if resp.status != 200:
+      return list
+  # checks that webpages have at least 250 words or less than 20000 words 
+  # https://wordcounter.net/words-per-page
+  if len(raw_response.content) < 250 or len(raw_response.content) > 45000:
+    return links
+  # use BeautifulSoup to extract links
+  soup = BeautifulSoup(resp.raw_response.content, 'lxml')
+  # checks if there is information
+  if soup is not None:
+    tokenize(url, soup)
+  for link in soup.find_all('a'):
+      l = link.get('href')
+      # makes sure link is valid
+      if l is not None:
+          # remove url fragment
+          l_defrag = l.split("#")
+          # checks for duplication and if url can be crawled
+          if l_defrag[0] not in visited_urls and is_valid(l_defrag[0]):
+              visited_urls.add(l_defrag[0])
+              list.append(l_defrag[0])
+  #set_list = set(list)
+  #list = [a for a in set_list]
+  return list
 
 def is_valid(url):
-    # Decide whether to crawl this url or not. 
-    # If you decide to crawl it, return True; otherwise return False.
-    # There are already some conditions that return False.
-    try:
-        parsed = urlparse(url)
-        # checks if scheme is valid
-        if parsed.scheme not in set(["http", "https"]):
-            return False
-        # checks if hostname is valid
-        if not any(parsed.netloc.endswith(domain) for domain in set(["ics.uci.edu", "cs.uci.edu", "informatics.uci.edu", 
-                                       "stat.uci.edu", "today.uci.edu"])):
-            return False
-        # checks if path is valid
-        if parsed.netloc.endswith("today.uci.edu") and "/department/information_computer_sciences/" not in parsed.path:
-            return False
-        # checks if urls are traps
-        if trap(url):
-            return False
-        
-        return not re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
-            + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
-            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-            + r"|epub|dll|cnf|tgz|sha1"
-            + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+  # Decide whether to crawl this url or not. 
+  # If you decide to crawl it, return True; otherwise return False.
+  # There are already some conditions that return False.
+  try:
+      parsed = urlparse(url)
+      # checks if scheme is valid
+      if parsed.scheme not in set(["http", "https"]):
+          return False
+      # checks if hostname is valid
+      if not any(parsed.netloc.endswith(domain) for domain in set(["ics.uci.edu", "cs.uci.edu", "informatics.uci.edu", 
+                                     "stat.uci.edu", "today.uci.edu"])):
+          return False
+      # checks if path is valid
+      if parsed.netloc.endswith("today.uci.edu") and "/department/information_computer_sciences/" not in parsed.path:
+          return False
+      # checks if urls are traps
+      if trap(url):
+          return False
 
-    except TypeError:
-        print ("TypeError for ", parsed)
-        raise
+      return not re.match(
+          r".*\.(css|js|bmp|gif|jpe?g|ico"
+          + r"|png|tiff?|mid|mp2|mp3|mp4"
+          + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+          + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+          + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+          + r"|epub|dll|cnf|tgz|sha1"
+          + r"|thmx|mso|arff|rtf|jar|csv"
+          + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+
+  except TypeError:
+      print ("TypeError for ", parsed)
+      raise
         
 def trap(url):
-    if "replyto" in url:
-        return True
-    if "calendar" in url: 
-        return True
-    if "?share=" in url:
-        return True
-    return False
+  # checks for sites with replyto in url
+  if "replyto" in url:
+      return True
+  # checks for sites with calendar in url
+  if "calendar" in url: 
+      return True
+  # checks for sites with ?share= in url
+  if "?share=" in url:
+      return True
+  return False
 
+# tokenizer
 def tokenize(url, soup):
   global longest_page
   global longest_word
   text = soup.get_text()
   result = re.split("[^a-zA-Z0-9']", text)
   result = list(filter(None, result))
+  # check and update longest_page and longest_word
   if len(result) > longest_word:
     longest_page = url
     longest_word = len(result)
+  # check and update word freqeuncies
   for word in result:
     if word not in stop_words:
       word_freq[word.lower()] += 1
@@ -126,8 +136,6 @@ def tokenize(url, soup):
     # longest page in terms of words
     # 50 most common words (ignore stop words)
     # number of subdomains (print URL, number)
-    
-    
 def create_report():
   # set of urls to avoid duplication of url with same domain and path
   global visited_urls 
@@ -137,7 +145,7 @@ def create_report():
   with open("Report.txt", mode="w") as f
     f.write("\nNumber of unique pages: " + str(len(visited_urls)))
     f.write("\nLongest page: " + longest_page + ", " + str(longest_word))
-    f.write("\n50 most common words: ")
+    f.write("\n50 most common words: \n")
     if len(word_freq) is not None:
       result = sorted(word_freq.items(), key=lambda x:x[1], reverse=True)
       count = 0
@@ -145,7 +153,6 @@ def create_report():
         if count < 50:
           f.write(word + ", " + str(total) + "\n")
           count += 1
-          
     f.write("\nics.uci.edu subdomains: ")
         for val, subdomain in enumerate(visited_urls):
             if subdomain != url:
